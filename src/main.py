ab4bf4b3bc3588cda8b077fcd5b8a7e10a814679
T8 <108910965+T888R@users.noreply.github.com>
2025-01-15 20:21:59 -0700

    add text node to html node

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from textnode import TextNode, TextType
 
+
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
+
 main()
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import LeafNode, ParentNode, HTMLNode
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
@@ -72,7 +73,7 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_to_html_many_children(self):
         node = ParentNode(
-            "p", 
+            "p",
             [
                 LeafNode("b", "Bold text"),
                 LeafNode(None, "Normal text"),
@@ -81,11 +82,10 @@ class TestHTMLNode(unittest.TestCase):
             ],
         )
         self.assertEqual(
-            node.to_html(), 
+            node.to_html(),
             "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
         )
 
-    
     def test_headings(self):
         node = ParentNode(
             "h2",
@@ -101,5 +101,7 @@ class TestHTMLNode(unittest.TestCase):
             "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
         )
 
+
 if __name__ == "__main__":
     unittest.main()
+
